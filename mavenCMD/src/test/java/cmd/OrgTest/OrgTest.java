79311c015c57190fd72aa62dc6234cbf033d2ea8
Gulshan Varshney <gulshanvarshney02@gmail.com>
2024-05-30 17:00:43 +0530

    first push

---
+++ mavenCMD/src/test/java/cmd/OrgTest/OrgTest.java
@@ -0,0 +1,16 @@
+package cmd.OrgTest;
+
+import org.testng.annotations.Test;
+
+public class OrgTest {
+
+	@Test
+	public void createOrg() {
+		System.out.println("createOrg");
+	}
+	
+	@Test
+	public void modifyOrg() {
+		System.out.println("modifyOrg");
+	}
+}
---
+++ mavenCMD/src/test/java/cmd/contactTest/ContactTest.java
@@ -0,0 +1,27 @@
+package cmd.contactTest;
+
+import org.testng.annotations.Test;
+
+public class ContactTest {
+
+	
+	@Test
+	public void createContact() {
+		String URL = System.getProperty("url");
+		String BROWSER = System.getProperty("browser");
+		String USERNAME = System.getProperty("username");
+		String PASSWORD = System.getProperty("password");
+		
+		System.out.println(URL);
+		System.out.println(BROWSER);
+		System.out.println(USERNAME);
+		System.out.println(PASSWORD);
+		
+		System.out.println("createContact");
+	}
+	
+	@Test
+	public void modifyContact() {
+		System.out.println("modifyContact");
+	}
+}
